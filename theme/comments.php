59422800159b2a976b415bcf683fd301cc80395f
Mazdak Pakaghideh <Mazdak.software@gmail.com>
2022-12-30 14:44:23 +0330

    Customized comment_form() for logged in users

--- theme/comments.php
+++ theme/comments.php
@@ -20,63 +20,8 @@ if ( post_password_required() ) {
 }
 ?>
 
-<div id="comments">
+<?php
+	$comments_args = array('class_submit' => 'comment-form ');
+	comment_form($comments_args);
+?>
 
-	<?php
-	// You can start editing here -- including this comment!
-	if ( have_comments() ) :
-		?>
-		<h2>
-			<?php
-			$mazdakdev_comment_count = get_comments_number();
-			if ( '1' === $mazdakdev_comment_count ) {
-				// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
-				printf(
-					/* translators: 1: title. */
-					esc_html__( 'One comment on &ldquo;%1$s&rdquo;', 'mazdakdev' ),
-					get_the_title()
-				);
-				// phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped
-			} else {
-				// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
-				printf(
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', $mazdakdev_comment_count, 'comments title', 'mazdakdev' ) ),
-					number_format_i18n( $mazdakdev_comment_count ),
-					get_the_title()
-				);
-				// phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped
-			}
-			?>
-		</h2>
-
-		<?php the_comments_navigation(); ?>
-
-		<ol>
-			<?php
-			wp_list_comments(
-				array(
-					'style'      => 'ol',
-					'callback'   => 'mazdakdev_html5_comment',
-					'short_ping' => true,
-				)
-			);
-			?>
-		</ol>
-
-		<?php
-		the_comments_navigation();
-
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() ) :
-			?>
-			<p><?php esc_html_e( 'Comments are closed.', 'mazdakdev' ); ?></p>
-			<?php
-		endif;
-
-	endif; // Check for have_comments().
-
-	comment_form();
-	?>
-
-</div><!-- #comments -->
--- theme/inc/template-functions.php
+++ theme/inc/template-functions.php
@@ -128,75 +128,8 @@ function mazdakdev_html5_comment( $comment, $args, $depth ) {
 		$moderation_note = __( 'Your comment is awaiting moderation. This is a preview; your comment will be visible after it has been approved.', 'mazdakdev' );
 	}
 	?>
+
 	<<?php echo esc_attr( $tag ); ?> id="comment-<?php comment_ID(); ?>" <?php comment_class( $comment->has_children ? 'parent' : '', $comment ); ?>>
-		<article id="div-comment-<?php comment_ID(); ?>" class="comment-body">
-			<footer class="comment-meta">
-				<div class="comment-author vcard">
-					<?php
-					if ( 0 !== $args['avatar_size'] ) {
-						echo get_avatar( $comment, $args['avatar_size'] );
-					}
-					?>
-					<?php
-					$comment_author = get_comment_author_link( $comment );
 
-					if ( '0' === $comment->comment_approved && ! $show_pending_links ) {
-						$comment_author = get_comment_author( $comment );
-					}
-
-					printf(
-						/* translators: %s: Comment author link. */
-						wp_kses_post( __( '%s <span class="says">says:</span>', 'mazdakdev' ) ),
-						sprintf( '<b class="fn">%s</b>', wp_kses_post( $comment_author ) )
-					);
-					?>
-				</div><!-- .comment-author -->
-
-				<div class="comment-metadata">
-					<?php
-					printf(
-						'<a href="%s"><time datetime="%s">%s</time></a>',
-						esc_url( get_comment_link( $comment, $args ) ),
-						esc_attr( get_comment_time( 'c' ) ),
-						esc_html(
-							sprintf(
-							/* translators: 1: Comment date, 2: Comment time. */
-								__( '%1$s at %2$s', 'mazdakdev' ),
-								get_comment_date( '', $comment ),
-								get_comment_time()
-							)
-						)
-					);
-
-					edit_comment_link( __( 'Edit', 'mazdakdev' ), ' <span class="edit-link">', '</span>' );
-					?>
-				</div><!-- .comment-metadata -->
-
-				<?php if ( '0' === $comment->comment_approved ) : ?>
-				<em class="comment-awaiting-moderation"><?php echo esc_html( $moderation_note ); ?></em>
-				<?php endif; ?>
-			</footer><!-- .comment-meta -->
-
-			<div class="comment-content prose">
-				<?php comment_text(); ?>
-			</div><!-- .comment-content -->
-
-			<?php
-			if ( '1' === $comment->comment_approved || $show_pending_links ) {
-				comment_reply_link(
-					array_merge(
-						$args,
-						array(
-							'add_below' => 'div-comment',
-							'depth'     => $depth,
-							'max_depth' => $args['max_depth'],
-							'before'    => '<div class="reply">',
-							'after'     => '</div>',
-						)
-					)
-				);
-			}
-			?>
-		</article><!-- .comment-body -->
 	<?php
 }
