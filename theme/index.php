8b73c709b2fe2a12b35b3ced31f948e1b0fe6913
Mazdak Pakaghideh <me@mazdak.dev>
2023-09-14 00:13:39 +0330

    a minor change in posts margin

--- theme/index.php
+++ theme/index.php
@@ -110,7 +110,7 @@ get_header();
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    class="h-6 w-6 ml-1"
+                    class="h-6 w-6 ml-1 mt-1"
                     >
 
                     <path
