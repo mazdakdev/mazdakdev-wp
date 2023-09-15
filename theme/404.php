fed15b95284e895e1f4e0d58c8a2b5853100940c
Mazdak Pakaghideh <me@mazdak.dev>
2023-09-15 12:48:34 +0330

    Changed 404 icon's size

--- theme/404.php
+++ theme/404.php
@@ -10,7 +10,8 @@
 get_header();
 ?>
     <div class="flex flex-col md:flex-row mt-8">
-      <img class="md:h-60 md:w-60 w-52 h-52 animate__animated animate__headShake self-center" src="https://mazdak.dev/assets/images/404.png" />
+      <img class="md:h-64 md:w-64 w-52 h-52 animate__animated animate__headShake self-center" src="https://mazdak.dev/assets/images/404.png" />
+
       <div class="self-center ml-4 text-center md:text-start">
         <strong class="text-4xl md:text-5xl text-gray-100">Page Not Found <i>!<i></strong>
         <div class="text-2xl text-gray-300">You've Got Lost.</div>
